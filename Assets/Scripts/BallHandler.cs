eaddd067724774ee53bdc88e40f144a25c097dd2
BurnEmDown <amitreuveni@gmail.com>
2023-01-30 02:52:51 +0200

    Added ability to drag ball and release to knock out the tower

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -6,7 +6,12 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D currentBallRigidbody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private float delayDuration;
+    
     private Camera mainCamera;
+    private bool isDragging;
 
     private void Start()
     {
@@ -15,12 +20,45 @@ public class BallHandler : MonoBehaviour
 
     void Update()
     {
-        if (!Touchscreen.current.primaryTouch.press.isPressed) return;
+        if(!currentBallRigidbody)
+            return;
+        
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+
+            isDragging = false;
+            return;
+        }
+
+        isDragging = true;
+        
+        currentBallRigidbody.isKinematic = true;
         
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        currentBallRigidbody.position = worldPosition;
+
         
-        Debug.Log(worldPosition);
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+        
+        Invoke(nameof(DetachBall), delayDuration);
+
+        
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
